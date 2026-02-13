638cd2bbd7a3d1967fec40633fb2ac2dad1a5ade
khawlaboulahrouf <khawlaboulahrouf@gmail.com>
2026-02-13 16:05:33 +0100

    add local storage

---
+++ js/storage.js
@@ -0,0 +1,8 @@
+export const Storage = {
+  get(key) {
+    return JSON.parse(localStorage.getItem(key)) || [];
+  },
+  set(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+};
